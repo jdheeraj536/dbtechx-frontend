7f81b2e6b1dda7db9d026f023214ffa5762acfbf
Dheeraj <jdheeraj536@gmail.com>
2026-06-11 11:24:14 +0530

    Standardized component names

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
